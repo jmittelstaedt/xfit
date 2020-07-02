8c674a791fc4e3c5e1401ee1ce077fa3507c297c
joseph <jam943@cornell.edu>
2020-07-02 08:36:29 -0400

    Moved to proper directory

---
+++ xfit/__init__.py
@@ -0,0 +1,3 @@
+from .fitting import fit_dataArray, fit_dataArray_models, fit_dataset, fit_dataset_models
+from .models import fitParameter, fitModel
+from .utils import coordinate_slice, da_filter, combine_new_ds_dim
